d91793fe41c6e7033752dd7333f94c5d0b4cce9d
k-neth <kneth46@gmail.com>
2026-05-15 09:58:18 +0300

    fix:  script for view count, category and pillar multiselect, script to set NS automatically on Learning hub

--- onerc_knowledge_hub/onerc_knowledge_hub/doctype/learning_hub/learning_hub.js
+++ onerc_knowledge_hub/onerc_knowledge_hub/doctype/learning_hub/learning_hub.js
@@ -6,3 +6,52 @@
 
 // 	},
 // });
+frappe.ui.form.on("Learning Hub", {
+    refresh(frm) {
+
+        if (!frm.is_new()) {
+
+            frappe.call({
+                method: "onerc_knowledge_hub.onerc_knowledge_hub.doctype.learning_hub.learning_hub.increment_view_count",
+                args: {
+                    docname: frm.doc.name
+                },
+                callback: function(r) {
+                    if (r.message) {
+                        frm.set_value("view_count", r.message);
+                    }
+                }
+            });
+
+        }
+    }
+});
+
+
+
+frappe.ui.form.on("Learning Hub", {
+    onload(frm) {
+
+        if (frm.is_new() && !frm.doc.posted_by) {
+
+            frappe.call({
+                method: "frappe.client.get_value",
+                args: {
+                    doctype: "Localization Hub User",
+                    filters: {
+                        user: frappe.session.user
+                    },
+                    fieldname: "name"
+                },
+                callback: function(r) {
+
+                    if (r.message) {
+                        frm.set_value("posted_by", r.message.name);
+                    }
+
+                }
+            });
+
+        }
+    }
+});
